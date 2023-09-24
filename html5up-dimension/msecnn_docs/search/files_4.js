7689f2610df4d0a3fc75d43eae7fee92103bfacc
raulkviana <73079422+raulkviana@users.noreply.github.com>
2023-09-24 22:14:45 +0100

    Update website
    * Add MSE-CNN documentation
    * Add Portifolio page (Still needs to look better)

---
+++ html5up-dimension/msecnn_docs/search/files_4.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['dataset_2emd_0',['dataset.md',['../dataset_8md.html',1,'']]],
+  ['dataset_5futils_2epy_1',['dataset_utils.py',['../dataset__utils_8py.html',1,'']]],
+  ['demo_2epy_2',['demo.py',['../demo_8py.html',1,'']]],
+  ['description_2emd_3',['description.md',['../description_8md.html',1,'']]]
+];
---
+++ html5up-dimension/msecnn_docs/search/pages_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['data_0',['Data',['../md_pages__doxygen_2dataset.html',1,'']]],
+  ['description_1',['description',['../md_src_2msecnn__raulkviana_2demo_2description.html',1,'']]]
+];
